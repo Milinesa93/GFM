29d036b4998c7cdae698de31dd44d889925db551
Milinesa93 <45246940+Milinesa93@users.noreply.github.com>
2024-12-18 16:12:21 +0100

    prueba color verde normal

--- dashboard.py
+++ dashboard.py
@@ -137,13 +137,11 @@ st.subheader("Outcomes by Q and Year")
 outcomes_by_q_year = data.groupby(['Outcome', 'Q', 'Year']).size().reset_index(name='Total')
 st.dataframe(outcomes_by_q_year)
 
-custom_color = '#02A95C'
-color_discrete_map = {year: custom_color for year in outcomes_by_q_year['Year'].unique()}
 
-# Graph: Outcomes by Q and Year with custom color
+
+# Graph: Outcomes by Q and Year
 fig_outcomes_q_year = px.bar(outcomes_by_q_year, x='Outcome', y='Total', color='Year',
-                             facet_col='Q', title="Outcomes by Q and Year",
-                             color_discrete_map=color_discrete_map)
+                             facet_col='Q', title="Outcomes by Q and Year", color_discrete_sequence=['green'])
 st.plotly_chart(fig_outcomes_q_year, use_container_width=True)
 
 # Total interviews by stage and Q-Year
